5adb3c26f6a03406361c8fbab35d9fcd699e593d
halilates <67061715+mazenzen@users.noreply.github.com>
2022-09-02 10:20:22 +0300

    2 Eylül

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -17,7 +17,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.DependencyResolvers.Autofac
+namespace Business.DependencyResolvers.Autofac 
 {
     public class AutofacBusinessModule : Module
     {
